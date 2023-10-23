8721f55993f9c08788308dbfecb552f1c6959b86
andrewtlu <andrewlulu2012@gmail.com>
2023-10-23 16:02:51 -0400

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/projectpandas/ridemory/RidemoryApplication.java
@@ -0,0 +1,13 @@
+package com.projectpandas.ridemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidemoryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projectpandas/ridemory/RidemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpandas.ridemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
