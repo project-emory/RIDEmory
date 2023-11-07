1d09136c9fa3ca384a13901b71ccca75c29b6d67
andrewtlu <andrewlulu2012@gmail.com>
2023-11-07 18:34:40 -0500

    Add pyngrok and secret parsing

--- bot/bot.py
+++ bot/bot.py
@@ -2,32 +2,56 @@ import os
 import requests
 
 from flask import Flask, request
+from pyngrok import ngrok
+
 
 app = Flask(__name__)
+app.config["TESTING"] = True
+bot_id = open("secret.txt", "r").read()
 
-@app.route('/', methods=['GET'])
+
+def init_webhooks(base_url):
+    pass
+
+
+@app.route("/", methods=["GET"])
 def home():
-    return 'You could put any content here you like, perhaps even a homepage for your bot!'
+    return (
+        "You could put any content here you like, perhaps even a homepage for your bot!"
+    )
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
+def sendmanual():
+    send(request.args.get("msg"))
+    return "ok", 200
+
+
+@app.route("/", methods=["POST"])
 def receive():
-    print('Incoming message:')
-    print(data)
+    print("Incoming message:")
+    print(request.data)
 
     # Prevent self-reply
-    if data['sender_type'] != 'bot':
-        if data['text'].startswith('/ping'):
-            send(data['name'] + ' pinged me!')
+    if request.data["sender_type"] != "bot":
+        if request.data["text"].startswith("/ping"):
+            send(request.data["name"] + " pinged me!")
 
-    return 'ok', 200
+    return "ok", 200
 
 
 def send(msg):
-    url  = 'https://api.groupme.com/v3/bots/post'
+    url = "https://api.groupme.com/v3/bots/post"
 
     data = {
-        'bot_id': os.getenv('BOT_ID'),
-        'text': msg,
+        "bot_id": bot_id,
+        "text": msg,
     }
+
     r = requests.post(url, data=data)
+
+
+public_url = ngrok.connect(5000).public_url
+print(public_url)
+app.config["BASE_URL"] = public_url
+init_webhooks(public_url)
