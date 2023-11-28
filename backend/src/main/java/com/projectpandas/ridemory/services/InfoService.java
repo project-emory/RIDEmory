9c358a21b8e4afc820c90ac08340de9fd9f33f26
andrewtlu <andrewlulu2012@gmail.com>
2023-11-28 00:09:07 -0500

    Implement basic wait time service

--- backend/src/main/java/com/projectpandas/ridemory/services/InfoService.java
+++ backend/src/main/java/com/projectpandas/ridemory/services/InfoService.java
@@ -38,6 +38,9 @@ public class InfoService {
         CompletableFuture<String> response = client.sendAsync(request, BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
 
+        // TODO: trim and process response to just return checkpoints and their wait
+        // times
+
         return response;
     }
 }
