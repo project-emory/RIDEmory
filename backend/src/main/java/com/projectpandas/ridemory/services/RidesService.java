f515cc9df36eb0e4f0cc3b118bd572b9bded2f91
Ethan Lee <ethan.lee@emory.edu>
2023-12-20 16:58:28 +0900

    Added ride services

--- backend/src/main/java/com/projectpandas/ridemory/services/RidesService.java
+++ backend/src/main/java/com/projectpandas/ridemory/services/RidesService.java
@@ -1,13 +1,53 @@
 package com.projectpandas.ridemory.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projectpandas.ridemory.models.Ride;
 import com.projectpandas.ridemory.repositories.RidesRepository;
 
 @Service
 public class RidesService {
     @Autowired
+    RidesRepository rides;
+
+    @Autowired
     RidesRepository repo;
-    
+
+    // CREATE
+    public Ride createRide(Ride ride) {
+        ride = rides.save(ride);
+        return ride;
+    }
+
+    // READ
+    public List<Ride> getRides() {
+        return rides.listRides(0, 10);
+    }
+
+    public Ride getRide(String id) {
+        return rides.findById(id).orElse(null);
+    }
+
+    // UPDATE
+    public Ride addRider(String id) {
+        Ride ride = rides.findById(id).orElse(null);
+        if (ride != null) {
+            ride.addRider();
+            rides.save(ride);
+        }
+        return ride;
+    }
+
+    // DELETE
+    public Ride deleteRide(String id) {
+        return rides.deleteRideById(id);
+    }
+
+    // SEARCH
+    public List<Ride> searchRides(long departTime, int riders) {
+        return rides.getRidesByFilter(departTime, riders);
+    }
 }
