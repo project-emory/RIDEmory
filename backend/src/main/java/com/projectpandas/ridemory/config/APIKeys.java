dca7d7737ec87f964ae039a375dde7472429ae37
andrewtlu <andrew.lu@emory.edu>
2024-03-22 14:57:25 -0400

    Fix leaked secret

---
+++ backend/src/main/java/com/projectpandas/ridemory/config/APIKeys.java
@@ -0,0 +1,14 @@
+package com.projectpandas.ridemory.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/secrets.properties")
+public class APIKeys {
+
+    @Value("${google.api.key}")
+    public static String googleAPIKey;
+
+}
--- backend/src/main/java/com/projectpandas/ridemory/services/InfoService.java
+++ backend/src/main/java/com/projectpandas/ridemory/services/InfoService.java
@@ -1,9 +1,11 @@
 package com.projectpandas.ridemory.services;
 
+import com.projectpandas.ridemory.config.APIKeys;
 import com.projectpandas.ridemory.models.Ride;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -27,7 +29,7 @@ public class InfoService {
     public final HttpClient client;
     public static final String TSAWaitTimeAPI = "https://www.atl.com/times/";
     public static final String[] checkpoints = { "MAIN", "NORTH", "LOWER NORTH", "SOUTH", "INT'L" };
-    private static final String GoogleMapAPIKEY = "AIzaSyA8kNTT8tbIbt4WGPDZNYDXC8HJX-EcPvs";
+    private static final String GoogleMapAPIKEY = APIKeys.googleAPIKey;
 
     // @Autowired
     public InfoService() {
@@ -67,14 +69,16 @@ public class InfoService {
         }
     }
 
-    public String  getTrafficTimeEstimate(Ride ride) {
-        List<Double> origin=ride.getTo();
-        List<Double> destination=ride.getFrom();
-//        GeoJsonPoint origin=ride.getOrigin();
-//        GeoJsonPoint destination=ride.getDestination();
-        try{
-            String url="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origin.get(0)+","+origin.get(1)+ "&destinations="+ destination.get(0) + "," + destination.get(1) + "&departure_time=now&traffic_model=best_guess&key="+GoogleMapAPIKEY;
-            HttpRequest request=HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+    public String getTrafficTimeEstimate(Ride ride) {
+        List<Double> origin = ride.getTo();
+        List<Double> destination = ride.getFrom();
+        // GeoJsonPoint origin=ride.getOrigin();
+        // GeoJsonPoint destination=ride.getDestination();
+        try {
+            String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + origin.get(0) + ","
+                    + origin.get(1) + "&destinations=" + destination.get(0) + "," + destination.get(1)
+                    + "&departure_time=now&traffic_model=best_guess&key=" + GoogleMapAPIKEY;
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() == 200) {
                 return response.body();
@@ -83,11 +87,10 @@ public class InfoService {
                 return null;
             }
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-
 
 }
