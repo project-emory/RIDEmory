5f7a9141663e6644d4d099f532fcb2da1baa0588
Ethan Lee <ethan.lee@emory.edu>
2024-04-11 20:29:31 -0400

    Setup react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import NavBar from "./components/NavBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 
 function App() {
 
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
