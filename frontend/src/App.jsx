b2a66942fb6b43da704e92acaec693c2780cc7da
Ethan Lee <ethan.lee@emory.edu>
2024-04-11 20:07:54 -0400

    Setup Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from "./components/NavBar"
+
 function App() {
 
   return (
     <>
-      
+      <NavBar />
     </>
   )
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <div>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <button>Log in</button>
+    </div>
+  );
+}
+ 
+export default NavBar;
