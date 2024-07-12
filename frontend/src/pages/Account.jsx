5853ad86f21ea53ecf5ae696df550c23a377ec46
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-07-11 20:07:33 -0400

    Created Account Page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,12 +1,34 @@
+import ConnectedAccounts from "../components/ConnectedAccounts.jsx";
 import NavBar from "../components/NavBar";
+import RecentRides from "../components/RecentRides.jsx";
 import UserProfile from "../components/UserProfile.jsx";
+import './Account.css';
 
 const Account = () => {
     return (
         <>
             <NavBar />
             <br />
-            <UserProfile />
+            
+            <div className="content-container">
+                <div className="top-section">
+                    <div>
+                        <UserProfile />
+                        <ConnectedAccounts />
+                    </div>
+                    
+                        
+                </div>
+                
+                <div className="bottom-section">
+                    <div className="recent-rides">
+                    
+                    <RecentRides />
+                    </div>
+                    
+                </div>
+            </div>
+            
         </>
     );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar.jsx";
+import Account from "./Account.jsx";
 
 const Home = () => {
     return (
         <>
-            <NavBar />
-            <h1>Home</h1>
+            { /*<!-- <NavBar /> --> */ }
+            { /* <h1>Home</h1> */ }
+            <Account />
         </>
     );
 };
