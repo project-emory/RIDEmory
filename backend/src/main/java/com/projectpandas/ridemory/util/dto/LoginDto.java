66c1b1d618ec994c493007e175df78452860aaec
andrewtlu <andrew.lu@emory.edu>
2024-07-25 21:41:04 -0400

    Add login/token dtos

---
+++ backend/src/main/java/com/projectpandas/ridemory/util/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.projectpandas.ridemory.util.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String email;
+    private String password;
+}
---
+++ backend/src/main/java/com/projectpandas/ridemory/util/dto/TokenDto.java
@@ -0,0 +1,8 @@
+package com.projectpandas.ridemory.util.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenDto {
+    private String token;
+}
